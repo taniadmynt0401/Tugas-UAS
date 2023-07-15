7ad87beea5f20770c8120155b71af579bd28b1f0
Tania Damayanti <127859384+taniadmynt0401@users.noreply.github.com>
2023-07-15 13:28:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Encapsulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketBookingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek TicketBooking
+            TicketBooking ticketBooking = new TicketBooking();
+
+            // Meminta pengguna untuk memasukkan informasi pemesanan tiket
+            Console.Write("Masukkan Nama: ");
+            string name = Console.ReadLine();
+            Console.Write("Masukkan Nomor Kartu Vaksin: ");
+            string vaccinationCardNumber = Console.ReadLine();
+            Console.Write("Masukkan NIK: ");
+            string nik = Console.ReadLine();
+            Console.Write("Masukkan Nomor Telepon: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Masukkan Nomor Kartu: ");
+            string cardNumber = Console.ReadLine();
+
+            // Mengatur nilai property menggunakan setter
+            ticketBooking.Name = name;
+            ticketBooking.VaccinationCardNumber = vaccinationCardNumber;
+            ticketBooking.NIK = nik;
+            ticketBooking.PhoneNumber = phoneNumber;
+            ticketBooking.CardNumber = cardNumber;
+
+            // Memanggil method BookTicket untuk memesan tiket
+            ticketBooking.BookTicket();
+        }
+    }
+}
---
+++ TicketBooking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+   
+       public class TicketBooking
+        {
+            // Variabel private
+            private string name;
+            private string vaccinationCardNumber;
+            private string nik;
+            private string phoneNumber;
+            private string cardNumber;
+
+            // Property untuk mengakses dan mengubah nilai name
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            // Property untuk mengakses dan mengubah nilai vaccinationCardNumber
+            public string VaccinationCardNumber
+            {
+                get { return vaccinationCardNumber; }
+                set { vaccinationCardNumber = value; }
+            }
+
+            // Property untuk mengakses dan mengubah nilai nik
+            public string NIK
+            {
+                get { return nik; }
+                set { nik = value; }
+            }
+
+            // Property untuk mengakses dan mengubah nilai phoneNumber
+            public string PhoneNumber
+            {
+                get { return phoneNumber; }
+                set { phoneNumber = value; }
+            }
+
+            // Property untuk mengakses dan mengubah nilai cardNumber
+            public string CardNumber
+            {
+                get { return cardNumber; }
+                set { cardNumber = value; }
+            }
+
+            // Method untuk memesan tiket dan menampilkan informasi pemesanan
+            public void BookTicket()
+            {
+                Console.WriteLine("Tiket berhasil dipesan untuk:");
+                Console.WriteLine("Nama: " + name);
+                Console.WriteLine("Nomor Kartu Vaksin: " + vaccinationCardNumber);
+                Console.WriteLine("NIK: " + nik);
+                Console.WriteLine("Nomor Telepon: " + phoneNumber);
+                Console.WriteLine("Nomor Kartu: " + cardNumber);
+            }
+        }
+    }
+
